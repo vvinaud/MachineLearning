41f99f4e1e8c3cea44aba744e64cee2e72441670
vvinaud <52412369+vvinaud@users.noreply.github.com>
2019-08-08 16:40:33 -0300

    update k-means
    For to base and base_final created to expose all the matrix;

--- agrupamento/k-means/k-means_exemplo1.py
+++ agrupamento/k-means/k-means_exemplo1.py
@@ -7,9 +7,13 @@ x=[20,  27,  21,  37,  46, 53, 55,  47,  52,  32,  39,  41,  39,  48,  48]
 y=[1000,1200,2900,1850,900,950,2000,2100,3000,5900,4100,5100,7000,5000,6500]  
 plt.scatter(x,y)
 
-base = np.array([[20,1000],[27,1200],[21,2900],[37,1850],[46,900],
-                 [53,950],[55,2000],[47,2100],[52,3000],[32,5900],
-                 [39,4100],[41,5100],[39,7000],[48,5000],[48,6500]])
+base = []
+for i in range(len(x)):
+ storage = np.array([x[i],y[i]])
+ base.append(storage)
+base = np.asarray(base) 
+del i, storage
+
 scaler = StandardScaler()
 base = scaler.fit_transform(base)
 
